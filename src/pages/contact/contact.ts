a8a025e6958f8a15df7f1afa19820b7029122ece
tanatat01 <tanatat253919@gmail.com>
2019-03-17 11:56:42 +0700

    up

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,7 +6,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  //items = ["Tanatat","Theiy","Sumare","Tanawid","Tanapong"];
 
+ items = [
+    {"name": "Tanatat","tel" : "0810060000","avatar" :"aaa.png" },
+    {"name": "Theiy","tel" : "0820000006","avatar" :"aaa.png" },
+    {"name": "Sumare","tel" : "0838020000","avatar" :"aaa.png" },
+    {"name": "Tanawid","tel" : "0840060000","avatar" :"aaa.png" },
+    {"name": "Tanapong","tel" : "0850240000","avatar" :"aaa.png" },
+ ];
+  itemSelected(item : string){
+    console.log("itemSelected Item : ",item);
+  }
   constructor(public navCtrl: NavController) {
 
   }
